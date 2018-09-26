96810459ae4f93b2ef9f6c01a5683cc80434df73
jrzlve <jrzlve@gmail.com>
2018-09-26 17:59:17 +0300

    v.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let zerosCount = 0;
+  for (let i = 5; number/i > 1; i *= 5) zerosCount += Math.floor(number/i);
+  return zerosCount;
 }
